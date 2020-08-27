6cb787ae071f2a3690c076854786726e4bd871ff
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-27 12:37:07 +0530

    page customized

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var dt = new Date();
+var options = {
+    timeZone: "Indian/Christmas",
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+};
+var nowDate = dt.toLocaleString('en-IN', options);
+document.querySelector('#date').innerHTML = nowDate;
