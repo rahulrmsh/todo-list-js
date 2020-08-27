97115bd3f082a01f48808f0fbff8d18b9a31ffa3
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-27 15:50:41 +0530

    version 1

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -10,3 +10,17 @@ var options = {
 };
 var nowDate = dt.toLocaleString('en-IN', options);
 document.querySelector('#date').innerHTML = nowDate;
+document.querySelector('#submit').addEventListener('click', function () {
+  var inputValue = document.querySelector('#listText').value;
+
+  if (inputValue !== null && inputValue !== null && inputValue !== undefined && inputValue !== 0 && inputValue !== "" && inputValue !== false && inputValue !== NaN) {
+    var innerDiv = document.createElement('div');
+    innerDiv.className = 'container-md themed-container text-muted';
+    innerDiv.textContent = inputValue;
+    document.querySelector('#listKey').appendChild(innerDiv);
+    document.querySelector('#listText').value = '';
+  }
+});
+document.querySelector('#reset').addEventListener('click', function () {
+  document.querySelector('#listKey').innerHTML = '';
+});
